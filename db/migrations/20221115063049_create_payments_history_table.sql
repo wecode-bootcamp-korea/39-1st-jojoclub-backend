5a13c9b4b8243871db1670b4c051804147805510
julius <julius@juliusui-MacBookAir.local>
2022-11-15 18:05:02 +0900

    [jojoclub_back] 초기환경설정 리뷰반영1

--- db/migrations/20221115063049_create_payments_history_table.sql
+++ db/migrations/20221115063049_create_payments_history_table.sql
@@ -1,11 +1,9 @@
 -- migrate:up
 CREATE TABLE payments_history (
   id INT NOT NULL AUTO_INCREMENT,
-  user_id INT NOT NULL,
   total_price DECIMAL(8,2),
   order_id INT NOT NULL,
   PRIMARY KEY (id),
-  CONSTRAINT payments_history_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id),
   CONSTRAINT payments_history_order_id_fkey FOREIGN KEY (order_id) REFERENCES orders (id)
 );
 
