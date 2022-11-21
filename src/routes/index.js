0787ffc39901aacd5a9232db1b1a9c2faa52065f
julius <julius@juliusui-MacBookAir.local>
2022-11-21 21:38:44 +0900

    merge conflict 해결

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const routes = express.Router();
+const userRouter = require("./user.router");
 const productRouter = require("./product.router");
 
+routes.use("/users", userRouter.routes)
 routes.use("/product", productRouter.routes);
 
 module.exports = { routes };
