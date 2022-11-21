caf4fa919e962cd9635e756ba09fe01bac408a6c
julius <julius@juliusui-MacBookAir.local>
2022-11-21 12:07:17 +0900

    제품상세, 전체 제품 조회, 필터링 API구현

--- src/routes/user.router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const routes = express.Router();
-const userController = require("../controllers/user.controller");
-
-routes.post("/signup", userController.signUp);
-routes.post("/signin", userController.signIn);
-
-module.exports = { routes };
