8b9be6876d998f9c5791c6f380053cf00ed8ed58
julius <julius@juliusui-MacBookAir.local>
2022-11-22 14:40:01 +0900

    wip

--- src/routes/order.router.js
+++ src/routes/order.router.js
@@ -4,7 +4,7 @@ const ordersController = require("../controllers/orders.controller");
 const {verifyToken} = require("../utils/verifyToken")
 
 routes.post("", verifyToken, ordersController.createOrders);
-routes.get("", verifyToken, ordersController);
+// routes.get("", verifyToken, ordersController);
 
 
 module.exports = { routes };
