d8a1c7462acaa572541bae4121f0b9da6277fcc3
julius <julius@juliusui-MacBookAir.local>
2022-11-22 15:32:11 +0900

    carts GET, POST, PATCH, DELETE API 구현

---
+++ src/controllers/carts.controller.js
@@ -0,0 +1,65 @@
+const cartsService = require("../services/carts.service");
+const { catchAsync } = require("../utils/error");
+
+const createCarts = catchAsync(async (req, res) => {
+  const { quantity, productOptionId } = req.body;
+  const userId = req.user.id;
+
+  if (!quantity || !userId || !productOptionId) {
+    const err = new Error("KEY_ERROR");
+    err.statusCode = 400;
+    throw err;
+  }
+
+  await cartsService.createCarts(quantity, userId, productOptionId);
+
+  return res.status(201).json({
+    message: "SIGNUP_SUCCESS",
+  });
+});
+
+const getCarts = catchAsync(async (req, res) => {
+  const userId = req.user.id;
+
+  if (!userId ) {
+    const err = new Error("KEY_ERROR");
+    err.statusCode = 400;
+    throw err;
+  }
+  
+  return res.status(201).json(await cartsService.getCarts(userId));
+});
+
+const updateCarts = catchAsync(async (req, res) => {
+  const { cartsId, quantity } = req.body;
+
+  if (!cartsId || !quantity ) {
+    const err = new Error("KEY_ERROR");
+    err.statusCode = 400;
+    throw err;
+  }
+
+  await cartsService.updateCarts(cartsId, quantity);
+
+  return res.status(201).json({
+    message: "UPDATE_SUCCESS",
+  });
+});
+
+const deleteCarts = catchAsync(async (req, res) => {
+  const { cartsId } = req.body;
+
+  if (!cartsId) {
+    const err = new Error("KEY_ERROR");
+    err.statusCode = 400;
+    throw err;
+  }
+
+  await cartsService.deleteCarts(cartsId);
+
+  return res.status(201).json({
+    message: "DELETE_SUCCESS",
+  });
+});
+
+module.exports = { createCarts, getCarts, updateCarts, deleteCarts };
---
+++ src/models/cart.dao.js
@@ -0,0 +1,65 @@
+const { appDataSource } = require("./data-source");
+
+const createCart = async (quantity, userId, productOptionId) => {
+  await appDataSource.query(
+    `
+    INSERT INTO carts (
+      quantity,
+      user_id,
+      product_option_id
+    ) VALUES (
+      ?,
+      ?,
+      ?
+    );
+    `,
+    [quantity, userId, productOptionId]
+  );
+};
+
+const getCart = async (userId) => {
+  const cart = await appDataSource.query(
+    `
+    SELECT 
+      u.id as userNum,
+      c.id as cartsNum,
+      po.image_url,
+      p.name_ko,
+      p.name_en,
+      s.name as size,
+      po.price,
+      c.quantity
+    FROM products p
+    INNER JOIN product_options po ON p.id = po.product_id
+    INNER JOIN sizes s ON s.id = po.size_id
+    INNER JOIN carts c ON c.product_option_id = po.id
+    INNER JOIN users u ON u.id = c.user_id
+    WHERE u.id = ?;
+    `,
+    [userId]
+  );
+  return cart;
+};
+
+const updateCart = async (cartsId, quantity) => {
+  await appDataSource.query(
+    `
+    UPDATE carts
+    SET quantity = ?
+    WHERE carts.id = ?;
+    `,
+    [quantity, cartsId]
+  );
+};
+
+const deleteCart = async (cartsId) => {
+  await appDataSource.query(
+    `
+    DELETE FROM carts
+    WHERE carts.id = ?;
+    `,
+    [cartsId]
+  );
+};
+
+module.exports = { createCart, getCart, updateCart, deleteCart};
