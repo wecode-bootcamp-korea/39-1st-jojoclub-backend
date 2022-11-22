a1bc6103ea1596de7a8623262acdab9019aac7f6
julius <julius@juliusui-MacBookAir.local>
2022-11-22 16:21:35 +0900

    product GET API 1차 수정

--- src/models/product.dao.js
+++ src/models/product.dao.js
@@ -11,8 +11,8 @@ const getNewProducts = async (orderBy, limitNum) => {
       products.created_at
     FROM products INNER JOIN product_options 
     ON products.id = product_id INNER JOIN sizes ON sizes.id = size_id
-    ORDER BY ${orderBy} limit ${limitNum};
-    `
+    ORDER BY ? limit ?;
+    `,[ orderBy, limitNum ]
   );
   return newProducts;
 };
