087551716028e15005da12f2f9173b5ffd68b196
julius <julius@juliusui-MacBookAir.local>
2022-11-24 02:08:31 +0900

    order GET API

--- src/models/order.dao.js
+++ src/models/order.dao.js
@@ -6,55 +6,33 @@ const getOrders = async (userId) => {
     `
     SELECT
       o.id orderId,
-      o.order_status_id orderStatus,
+      os.name orderStatus,
       o.total_price totalPrice,
       o.created_at createdAt,
         JSON_ARRAYAGG(
         JSON_OBJECT(
         "productName", p.name_ko,
         "quantity", oi.quantity,
-        "orderItemStatus", oi.order_item_status_id,
+        "orderItemStatus", ois.name,
         "productOptionId", oi.product_option_id,
         "price", po.price,
-        "size", po.size_id,
+        "size", s.name,
         "image", po.image_url
         )
       ) orderProduct
     FROM orders o
-    LEFT JOIN order_items oi ON o.id = oi.order_id
+    INNER JOIN order_items oi ON o.id = oi.order_id
+    LEFT JOIN order_items_status ois ON ois.id = oi.order_item_status_id
+    LEFT JOIN order_status os ON os.id = o.order_status_id
     LEFT JOIN product_options po ON po.id = oi.product_option_id
     LEFT JOIN products p ON po.product_id = p.id
     LEFT JOIN sizes s ON s.id = po.size_id
     WHERE o.user_id = 1
     GROUP BY o.id
+    ORDER By o.created_at DESC;
     `,
     [userId]
   ); 
 };
 
 module.exports = { getOrders };
-
-// `
-// SELECT
-//   u.id
-//   u.name,
-//   product_options.
-//   products.name,
-//   product_options.price,
-//   order_items.quantity,
-//   order_items_status.name,
-//   orders.created_at,
-//   orders.total_price,
-//   order_status.name
-// FROM orders o
-// LEFT JOIN users u ON u.id = o.user_id
-// LEFT JOIN order_items oi ON o.id = oi.order_id
-// LEFT JOIN product_options po ON po.id = oi.product_option_id
-// LEFT JOIN products p ON p.id = po.produc_id
-// LEFT JOIN order_status os ON os.id = o.order_status_id
-// LEFT JOIN order_items_status ois ON ois.id = oi.order_item_status_id
-// LEFT JOIN sizes s ON s.id = po.size_id
-// WHERE o.user_id = 1;
-
-// `,
-// [orderStatusId, userId]
