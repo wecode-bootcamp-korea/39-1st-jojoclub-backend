5ea417ce59db23c903644be72580a641aebc2ae2
julius <julius@juliusui-MacBookAir.local>
2022-11-24 02:32:39 +0900

    order GET API

--- src/models/order.dao.js
+++ src/models/order.dao.js
@@ -13,20 +13,18 @@ const getOrders = async (userId) => {
         JSON_OBJECT(
         "productName", p.name_ko,
         "quantity", oi.quantity,
-        "orderItemStatus", ois.name,
+        "orderItemStatus", oi.order_item_status_id,
         "productOptionId", oi.product_option_id,
         "price", po.price,
-        "size", s.name,
+        "size", po.size_id,
         "image", po.image_url
         )
       ) orderProduct
     FROM orders o
     INNER JOIN order_items oi ON o.id = oi.order_id
-    LEFT JOIN order_items_status ois ON ois.id = oi.order_item_status_id
     LEFT JOIN order_status os ON os.id = o.order_status_id
     LEFT JOIN product_options po ON po.id = oi.product_option_id
     LEFT JOIN products p ON po.product_id = p.id
-    LEFT JOIN sizes s ON s.id = po.size_id
     WHERE o.user_id = 1
     GROUP BY o.id
     ORDER By o.created_at DESC;
