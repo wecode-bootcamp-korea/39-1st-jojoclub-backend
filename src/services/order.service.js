5fd4791f171b1d46a2fce18a42437a037b19d44f
julius <julius@juliusui-MacBookAir.local>
2022-11-24 18:06:12 +0900

    order POST API conflict 해결

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -4,7 +4,6 @@ const getOrder = async (userId) => {
   return await orderDao.getOrders(userId);
 };
 
-module.exports = {}
 const createOrder = async (productOptionId, quantity, totalPrice, userId) => {
   return await orderDao.createOrder(productOptionId, quantity, totalPrice, userId);
 };
